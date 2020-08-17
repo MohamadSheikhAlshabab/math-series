88b3773dcf1182dd087bf7c70f406d0b91179121
MohamadSheikhAlshabab <alshabab.moh@gmail.com>
2020-08-17 16:33:22 +0300

    TDD programming with Fabonacci & Lucas series

--- math_series/series.py
+++ math_series/series.py
@@ -1,22 +1,20 @@
-def lucas_check(num):
+def lucas_check(n):
     if num==0:
         return 2
     elif num==1:
         return 1
     elif num>1:
-        return lucas_check(num-1)+lucas_check(num-2)
-    print(num)
+        return lucas_check(n-1)+lucas_check(n-2)
 
 
-def fibonacci(num):
+def fibonacci(n):
     if  num == 0 :
         return 0
     elif num == 1 :
         return 1
     elif num > 1 :
-        return fibonacci ( num - 1 ) + fibonacci ( num - 2)
-    print(num)
+        return fibonacci ( n - 1 ) + fibonacci ( n - 2)
         
 
-def sum_series(num,num2=0, num3=1):
-    return lucas_check(num)+fibonacci(num)
+def sum_series(n,n2=0, n3=1):
+    return lucas_check(n)+fibonacci(n)
